facca01c23fc02b765d30996b07a7c96c6bf6dc6
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2023-04-18 22:18:06 +0200

    added ts library

---
+++ angular-15/nx/myorg/apps/admin/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'myorg-root',
+  template: `
+  <myorg-banner title="Welcome to our admin app."> </myorg-banner>
+  `,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'admin';
+}
